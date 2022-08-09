8835888a3f8c73c2d513b8f9a524b7fdb419ad4f
srikarrb12 <srikarrb12@gmail.com>
2022-08-09 11:24:07 -0400

    Changed skills

--- src/Skills.js
+++ src/Skills.js
@@ -4,9 +4,9 @@ export default function Skills(props) {
   return (
     <>
       <SkillsEntry sectionTitle="Skills" skillOne="Java" skillTwo="Python" skillThree="JavaScript" />
-      <SkillsEntry skillOne="HTML" skillTwo="CSS" skillThree="Docker" />
-      <SkillsEntry skillOne="MySQL" skillTwo="SQL" skillThree="ECS" />
-      <SkillsEntry skillOne="REST APIs" skillTwo="DynamoDB" skillThree="ReactJS"/>
+      <SkillsEntry skillOne="HTML" skillTwo="CSS" skillThree="ReactJS" />
+      <SkillsEntry skillOne="MySQL" skillTwo="SQL" skillThree="AWS ECS" />
+      <SkillsEntry skillOne="REST APIs" skillTwo="DynamoDB" skillThree="Docker"/>
       <SkillsEntry skillOne="API Gateway" skillTwo="Lambda" skillThree="TypeScript"/>
       <SkillsEntry skillOne="Terraform" skillTwo="Git"/>
     </>
