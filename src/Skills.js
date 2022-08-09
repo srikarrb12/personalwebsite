a7cbb7c7a34a8fa19956a5af926b64d56509f118
srikarrb12 <srikarrb12@gmail.com>
2022-08-09 11:21:47 -0400

    Changed skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,15 +1,14 @@
-import { Col, Row } from "react-bootstrap";
 import SkillsEntry from "./SkillsEntry";
 
 export default function Skills(props) {
   return (
     <>
-      <SkillsEntry skillOne="Java" skillTwo="Python" skillThree="JavaScript" sectionTitle="Skills" />
+      <SkillsEntry sectionTitle="Skills" skillOne="Java" skillTwo="Python" skillThree="JavaScript" />
       <SkillsEntry skillOne="HTML" skillTwo="CSS" skillThree="Docker" />
-      <SkillsEntry skillOne="MySQL" skillTwo="SQL" skillThree="AWS" />
+      <SkillsEntry skillOne="MySQL" skillTwo="SQL" skillThree="ECS" />
       <SkillsEntry skillOne="REST APIs" skillTwo="DynamoDB" skillThree="ReactJS"/>
       <SkillsEntry skillOne="API Gateway" skillTwo="Lambda" skillThree="TypeScript"/>
-      <SkillsEntry skillOne="Terraform" skillTwo="Pinpoint"/>
+      <SkillsEntry skillOne="Terraform" skillTwo="Git"/>
     </>
   );
 }
